29520e36d274bfe057b2309f4654a38c173e1538
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-07-28 22:18:22 -0300

    Primeira interação com C# e .NET
    Meu primeiro contato com a linguagem C# e o framework dotnet.

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,7 @@
+﻿//No prompt de comando Terminal, para criar o novo aplicativo de console na pasta especificada, digite dotnet new console -o ./CsharpProjects/TestProject e pressione Enter
+
+//para executar um programa tem que clicar com o botão direito do mouse no Program.cs e em open Integraded Terminal e por fim escrever dotnet run no terminal
+Console.WriteLine("Hello word!");
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
